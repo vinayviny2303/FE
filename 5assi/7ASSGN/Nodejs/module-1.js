dcfc97f4343751d6a46a8aa215de64342cb4185e
Vinay <vinayviny70@gmail.com>
2022-09-20 18:38:42 +0530

    first commit

---
+++ 5assi/7ASSGN/Nodejs/module-1.js
@@ -0,0 +1,13 @@
+
+
+const person={
+    id:200,
+    name:'vinay'
+    rollno:20071a12h0,
+    course:'it'
+}
+
+const arr=[20,30,40,50]
+
+//export
+module.exports = {person,arr}
---
+++ 5assi/7ASSGN/Nodejs/module-2.js
@@ -0,0 +1,5 @@
+
+//import module-1
+let x=require('./module-1')
+
+console.log("x is",x)
